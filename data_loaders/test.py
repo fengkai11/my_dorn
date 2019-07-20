da01205e8fa102faf05b40020b7715437bdf59de
fengkai11 <1090050820@qq.com>
2019-07-20 11:27:02 +0800

    network done
    Signed-off-by: feng <1090050820@qq.com>

--- data_loaders/test.py
+++ data_loaders/test.py
@@ -1,7 +1,16 @@
-from scipy import misc
-f = misc.face()
-misc.imsave('face.png',f)
-import matplotlib.pyplot as plt
-plt.imshow(f)
-plt.axis('off')
-plt.show()
+# import torchvision
+# from torchvision import models, transforms
+#
+# model = models.resnet101(pretrained=True)
+# import torch
+# saved_state_dict = torch.load('../resnet101-5d3b4d8f.pth')
+# for i in saved_state_dict:
+#     print(i)
+
+import torch
+a = torch.randn((2,3))
+b = torch.randn((1,3))
+c = torch.cat((a,b),dim = 0)
+print(a)
+print(b)
+print(c)
--- network/backbone.py
+++ network/backbone.py
@@ -6,7 +6,7 @@ affine_par = True
 #size was not changed
 def conv3x3(in_planes,out_planes,stride = 1):
     return nn.Conv2d(in_planes,out_planes,kernel_size=3,stride=stride,
-                     pad = 1,bias=False)
+                     padding = 1,bias=False)
 class Bottleneck(nn.Module):
     expansion=4
 
@@ -14,14 +14,14 @@ class Bottleneck(nn.Module):
         super(Bottleneck,self).__init__()
         self.conv1 = nn.Conv2d(inplanes,planes,kernel_size=1,bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplanes = False)
+        self.relu = nn.ReLU(inplace = False)
         self.conv2 = nn.Conv2d(planes,planes,kernel_size=3,stride=stride,
                                padding= dilation*multi_grid,dilation = dilation*multi_grid,bias = False)
         self.bn2 = BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes,planes*4,kernel_size=1,bias=False)
         self.bn3 = BatchNorm2d(planes*4)
-        self.relu = nn.ReLU(inplanes=False)
-        self.relu_inplace = nn.ReLU(inplanes=True)
+        self.relu = nn.ReLU(inplace=False)
+        self.relu_inplace = nn.ReLU(inplace=True)
         self.downsample= downsample
         self.dilation = dilation
         self.stride = stride
@@ -52,16 +52,22 @@ class ResNet(nn.Module):
         super(ResNet,self).__init__()
         self.conv1 = conv3x3(3,64,stride=2)
         self.bn1 = BatchNorm2d(64)
-        self.relu1 = nn.ReLU(inplace=True)
+        self.relu1 = nn.ReLU(inplace=False)
         self.conv2 = conv3x3(64,64)
         self.bn2 = BatchNorm2d(64)
-        self.relu2 = nn.ReLU(inplace=True)
+        self.relu2 = nn.ReLU(inplace=False)
         self.conv3 = conv3x3(64,128)
         self.bn3 = BatchNorm2d(128)
-        self.relu3 = nn.ReLU(inplace=True)
+        self.relu3 = nn.ReLU(inplace=False)
         self.maxpool = nn.MaxPool2d(kernel_size=3,stride = 2,padding =1)
-        self.relu = nn.ReLU(inplace=True)
-    def _make_layer(self,block,planes,blocks,stride=1,dilation = 1,multi_grad = 1):
+        self.relu = nn.ReLU(inplace=False)
+        self.layer1 = self._make_layer(block,64,layers[0])
+        self.layer2 = self._make_layer(block,128,layers[1],stride=2)
+        self.layer3 = self._make_layer(block,256,layers[2],stride=1,dilation = 2)
+        self.layer4 = self._make_layer(block,512,layers[3],stride=1,dilation = 4,multi_grid = (1,1,1))
+
+
+    def _make_layer(self,block,planes,blocks,stride=1,dilation = 1,multi_grid = 1):
         downsample = None
         if stride !=1 or self.inplanes != planes*block.expansion:
             downsample = nn.Sequential(
@@ -69,5 +75,38 @@ class ResNet(nn.Module):
                           kernel_size=1,stride=stride,bias=False),
                 BatchNorm2d(planes*block.expansion,affine=affine_par)
             )
+        layers = []
+        generate_multi_grid = lambda index,grids:grids[index%len(grids)] if isinstance(grids,tuple) else 1
+        layers.append(block(self.inplanes,planes,stride,dilation = dilation,downsample = downsample,
+                            multi_grid = generate_multi_grid(0,multi_grid)))
+        self.inplanes = planes*block.expansion
+        for i in range(1,blocks):
+            layers.append(
+                block(self.inplanes,planes,dilation = dilation,multi_grid = generate_multi_grid(i,multi_grid))
+            )
+        return nn.Sequential(*layers)
+    def forward(self,x):
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.relu2(self.bn2(self.conv2(x)))
+        x = self.relu3(self.bn3(self.conv3(x)))
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
+
+    #TODO implenment freez( )
+def resnet101(pretrained=True):
+    resnet101 = ResNet(Bottleneck,[3,4,33,3])
+    if pretrained:
+        saved_state_dict = torch.load('../resnet101-5d3b4d8f.pth')
+        new_params = resnet101.state_dict().copy()
+        for i in saved_state_dict:
+            i_parts = i.split('.')
+            if not i_parts[0]== 'fc':
+                new_params['.'.joint(i_parts[0])] = saved_state_dict[i]
+        resnet101.load_state_dict(new_params)
+    return resnet101
 
 
